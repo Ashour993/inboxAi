7f470b084ece6f19976866721c77c3118a3bb867
Ashour993 <ash_g_u_51@hotmail.com>
2026-05-25 21:50:38 +0200

    improved gitignore setup

---
+++ src/test/java/com/inboxai/inboxai/InboxaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inboxai.inboxai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InboxaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
